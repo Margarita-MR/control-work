abb73501b3a8f76c91759849ea5997826b470044
MargaritaM <ya.margarita1005@gmail.com>
2022-11-29 21:59:21 +0300

    Написала метод 1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] array1 = new string[5] {"dog", "Russia", "hello", "world", "cat"};
 string[] array2 = new string[array1.Length];
-void SecondArrayWithIF(string[] array1, string[] array2)
+void  SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
