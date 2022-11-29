505ecc71f318d16495a391476c6b26a106bfb555
MargaritaM <ya.margarita1005@gmail.com>
2022-11-29 22:26:37 +0300

    Окончательный результат

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] array1 = new string[5] {"dog", "Russia", "hello", "world", "cat"};
 string[] array2 = new string[array1.Length];
-void  SecondArrayWithIF(string[] array1, string[] array2)
+void  SizeArray(string[] array1, string[] array2)
 {
     int count = 0;
     for (int i = 0; i < array1.Length; i++)
@@ -20,5 +20,5 @@ void PrintArray(string[] array)
     }
     Console.WriteLine();
 }
-SecondArrayWithIF(array1, array2);
+SizeArray(array1, array2);
 PrintArray(array2);
